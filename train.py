944c589aa6c62b0b1d31d95b284748f974cc22f5
cleantest <113685819+cleantest@users.noreply.github.com>
2025-11-02 14:19:54 +0400

    Added runs folder with best model and training results

--- train.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Load a pretrained YOLOv8 model (tiny version for speed)
-model = YOLO('yolov8n.pt')
-
-# Train on your custom dataset
-model.train(data='data.yaml', epochs=30, imgsz=416, batch=16)
